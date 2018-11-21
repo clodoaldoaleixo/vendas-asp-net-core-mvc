f984aadeff03fb535f11dfd622c81031b7adfd34
Clodoaldo Aleixo <clodoaldoaleixo@hotmail.com>
2018-11-21 15:37:58 -0200

    Adicionando Departamento

---
+++ VendasMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VendasMVC.Models;
+
+namespace VendasMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> listaDepartamentos = new List<Departamento>();
+            listaDepartamentos.Add(new Departamento { Id = 1, Nome = "Eletrônico" });
+            listaDepartamentos.Add(new Departamento { Id = 2, Nome = "Moda" });
+            
+            return View(listaDepartamentos);
+        }
+    }
+}
---
+++ VendasMVC/Models/Departamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace VendasMVC.Models
+{
+    [Serializable]
+    public class Departamento
+    {
+        [DisplayName("Código")]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
