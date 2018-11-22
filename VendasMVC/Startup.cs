0499845a827b849fec1b638ba0d3f0d244c0f6f8
Clodoaldo Aleixo <clodoaldoaleixo@hotmail.com>
2018-11-22 13:28:45 -0200

    Adicionando Estilo

--- VendasMVC/Startup.cs
+++ VendasMVC/Startup.cs
@@ -37,7 +37,7 @@ namespace VendasMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<VendasMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("VendasMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("VendasMVCContext"), builder => builder.MigrationsAssembly("VendasMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
