28c5437a59ff4aa0b5e77e02e49d3f205acc313d
Clodoaldo Aleixo <clodoaldoaleixo@hotmail.com>
2018-11-22 15:21:39 -0200

    Adicionando menu

---
+++ VendasMVC/Controllers/VendedoresController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VendasMVC.Models;
+
+namespace VendasMVC.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
